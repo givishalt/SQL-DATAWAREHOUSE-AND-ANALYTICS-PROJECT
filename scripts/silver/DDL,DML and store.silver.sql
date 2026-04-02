84b544181026566fc464d22ad7e3577df92c2639
Vishal <VISHAL9681032@GMAIL.COM>
2026-04-02 13:50:48 +0530

    Add files via upload

--- scripts/silver/DDL,DML and store.silver.sql
+++ scripts/silver/DDL,DML and store.silver.sql
@@ -235,38 +235,48 @@ sls_sales,
 sls_quantity,
 sls_price)
 
-select 
-      sls_ord_num,
-      sls_prd_key,
-      sls_cust_id,
-      case 
-            when sls_order_dt=0 or len(sls_order_dt) !=8 then null
-            else cast(cast(sls_order_dt as varchar) as date)
-            end as sls_order_dt,
+select
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
 
-       case
-            when  sls_due_dt=0 or len(sls_due_dt) !=8 then null
-            else cast(cast(sls_due_dt as varchar) as date) 
-            end as sls_due_dt,
+    case
+        when sls_order_dt = 0 or len(sls_order_dt) != 8 then null
+        else cast(cast(sls_order_dt as varchar) as date)
+    end as sls_order_dt,
+
+    case
+        when sls_due_dt = 0 or len(sls_due_dt) != 8 then null
+        else cast(cast(sls_due_dt as varchar) as date)
+    end as sls_due_dt,
+
+    case
+        when sls_ship_dt = 0 or len(sls_ship_dt) != 8 then null
+        else cast(cast(sls_ship_dt as varchar) as date)
+    end as sls_ship_dt,
+
+    case
+        when sls_sales is null 
+          or sls_sales <= 0
+          or sls_sales != sls_quantity * abs(sls_price)
+        then sls_quantity * abs(sls_price)
+        else sls_sales
+    end as sls_sales,
+
+    sls_quantity,
+
+    case
+        when sls_price is null or sls_price <= 0
+        then sls_sales / nullif(sls_quantity, 0)
+        else sls_price
+    end as sls_price
+
+from bronze.crm_sales_details
 
 
-      case 
-            when sls_ship_dt=0 or len(sls_ship_dt) != 8 then null
-            else cast(cast(sls_ship_dt as varchar) as date) 
-            end as sls_ship_dt,
-      
-      case 
-            when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
-            then sls_quantity * abs(sls_price)
-            else sls_sales 
-            end as  sls_sales ,
-sls_quantity,
-
-      case 
-            when sls_price is null or sls_price <=0 then sls_sales/ nullif(sls_quantity , 0)
-            else sls_price  
-            end as sls_price 
-from bronze.crm_sales_details;  ------- error in this code what's wrong here 
+select count(*) from silver.crm_sales_details  -- should be > 0
+select top 5 *  from silver.crm_sales_details  -- dates should look like 2021-01-15  
+  
 
 select *  from bronze.crm_sales_details
 
